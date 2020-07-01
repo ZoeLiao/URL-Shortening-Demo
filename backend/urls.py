478e7682a92b0c37eef36f454df0c0c7147a054a
ZoeLiao <zoejoyuliao@gmail.com>
2020-07-01 09:55:45 +0800

    Fix static files not found

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,9 @@ from django.urls import (
     path,
     re_path
 )
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import permissions
 
 from drf_yasg.views import get_schema_view
@@ -50,4 +53,4 @@ urlpatterns += [
    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
